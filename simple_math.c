c6bac9994835bbbb92170a0aa12d08570e3d0146
sshzuff <sshz2004@gmail.com>
2025-02-27 13:27:42 +0100

    first version of simple math - no test

---
+++ simple_math.c
@@ -0,0 +1,10 @@
+/** \file simple_math.c
+    \brief definition of the funct
+    \author Sara<
+ */
+
+ #include "simple_math.h"
+
+ float mean(int a, int b) {
+    return (a+b)/2;
+ }
---
+++ simple_math.h
@@ -0,0 +1,11 @@
+/** \file simple_math.h
+    \brief a toy example of math library
+    \author Sara 
+*/
+
+#ifndef _SIMPLEMATH_
+#define _SIMPLEMATH_
+
+float mean (int a, int b);
+
+#endif 
