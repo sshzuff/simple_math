9a0d5b17e9ea3b7af07bf4c821d35eec45ac6e81
sshzuff <sshz2004@gmail.com>
2025-03-04 12:28:48 +0100

    hotfix of mean function

--- simple_math.c
+++ simple_math.c
@@ -5,8 +5,9 @@
 
  #include "simple_math.h"
 
+ //per le regole del c stiamo facendo numero intero fratto numero intero
 float mean(int a, int b) {
-    return (a+b)/2;
+    return (a+b)/2.0;
  }
 
  int max (int a, int b) {
