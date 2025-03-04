42d058e0e177edfedc9b13d49d22ab5d85e8ec95
sshzuff <sshz2004@gmail.com>
2025-03-04 12:16:38 +0100

    with complex numbers - tested

--- simple_math.c
+++ simple_math.c
@@ -8,3 +8,22 @@
 float mean(int a, int b) {
     return (a+b)/2;
  }
+
+ int max(int a, int b) {
+    if(a>b){
+        return a;
+    }
+    else {
+        return b;
+    }
+ }
+
+ cx_float_t cx_mean(cx_int_t a, cx_int_t b){
+    
+    cx_float_t average;
+    
+    average.re = mean (a.re, b.re);
+    average.imm = mean(a.imm, b.imm);
+    
+    return average;
+ }
